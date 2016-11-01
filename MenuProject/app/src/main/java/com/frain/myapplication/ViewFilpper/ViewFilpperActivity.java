565e36e8afed95602815bf9583fa79ce33285465
furuiCQ <1182590636@qq.com>
2016-11-01 09:10:47 +0800

    10.24-10。28Fragment的知识点

---
+++ MenuProject/app/src/main/java/com/frain/myapplication/ViewFilpper/ViewFilpperActivity.java
@@ -0,0 +1,23 @@
+package com.frain.myapplication.ViewFilpper;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ViewFlipper;
+
+import com.frain.myapplication.R;
+
+/**
+ * Created by admin on 2016/11/1.
+ */
+public class ViewFilpperActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_viewfillper);
+
+        ViewFlipper viewFilpper;
+
+
+
+    }
+}
